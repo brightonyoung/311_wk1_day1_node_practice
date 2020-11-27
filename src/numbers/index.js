746133635f7521c77295b91eca1c8132833bb395
brightonyoung <brighton.young@outlook.com>
2020-11-27 03:38:56 -0600

    first commit

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,32 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+// if(num % 2 === 0) {
+//   return true;
+// }
+//or
+return num % 2 === 0 ? true : false;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
+  var sum = arr.reduce(function(a,b) {
+    return a + b;
+  })
+  return sum;
+} 
 
-}
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  var total = arr.reduce(function(a,b) {
+    return a + b;
+  })
+  if (total == sum) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,21 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  let array = str.split("-");
+  return array;
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let pairs = str.match(/(..?)/g);
+  return pairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let array = str.split('');
+  let reverse = array.reverse();
+  let string = reverse.join("");
+  return string;
 }
 
 module.exports = {
